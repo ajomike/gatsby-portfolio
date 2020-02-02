3239f6577c2a12c06c5dd1a85acae7461c044832
Michael Escoto <michael.escoto@parsus.com>
2020-02-02 14:20:35 -0700

    chore(projects.js): added projects json file

---
+++ src/projects.js
@@ -0,0 +1,32 @@
+module.exports = [
+  {
+    title: 'Cliffs Hotel and Spa',
+    image: 'https://res.cloudinary.com/dssctsifq/image/upload/c_scale,w_768/v1555188691/portfolio/cliffshotelandspa.png',
+    link: 'https://cliffshotelandspa.com'
+  },
+  {
+    title: 'Spinato\'s Pizzeria',
+    image: 'https://res.cloudinary.com/dssctsifq/image/upload/c_scale,w_768/v1555188688/portfolio/spinatospizzeria.png',
+    link: 'https://spinatospizzeria.com'
+  },
+  {
+    title: 'Loft & Manor',
+    image: 'https://res.cloudinary.com/dssctsifq/image/upload/c_scale,w_768/v1555188690/portfolio/loftandmanor.png',
+    link: 'https://loftandmanor.com'
+  },
+  {
+    title: 'Safeguard Security',
+    image: 'https://res.cloudinary.com/dssctsifq/image/upload/c_scale,w_768/v1555188688/portfolio/safeguardsecurity.png',
+    link: 'https://www.safeguardsecurity.com/'
+  },
+  {
+    title: 'Tilted Kilt',
+    image: 'https://res.cloudinary.com/dssctsifq/image/upload/c_scale,w_768/v1555188690/portfolio/tiltedkilt.com.png',
+    link: 'https://tiltedkilt.com/franchising/'
+  },
+  {
+    title: 'Dr. Gingers',
+    image: 'https://res.cloudinary.com/dssctsifq/image/upload/c_scale,w_768/v1555190524/portfolio/drgingers.png',
+    link: 'https://drgingers.com'
+  }
+];
