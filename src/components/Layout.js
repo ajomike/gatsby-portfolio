b6155bac431eb685c1c59510c88453965be35936
Michael Escoto <michael.escoto@parsus.com>
2020-02-02 14:11:46 -0700

    refactor(meta.js): refactor component to use props

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,16 +1,35 @@
-import React from "react"
+import React from "react";
+import { useStaticQuery, graphql } from 'gatsby';
 
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 import Meta from '../components/Meta'
 
-const Layout = ({children}) => (
-  <main>
-    <Meta />
-    <Header />
-    {children}
-    <Footer />
-  </main>
-)
+const Layout = ({children}) => {
+  const data = useStaticQuery(graphql`
+    query SiteTitleQuery {
+      site {
+        siteMetadata {
+          title
+          description
+          siteUrl
+        }
+      }
+    }
+  `);
+  
+  return (
+    <main>
+      <Meta 
+        siteTitle={data.site.siteMetadata.title}
+        siteDescription={data.site.siteMetadata.description}
+        siteUrl={data.site.siteMetadata.siteUrl}
+      />
+      <Header />
+      {children}
+      <Footer />
+    </main>
+  )
+}
 
 export default Layout;
--- src/components/Meta.js
+++ src/components/Meta.js
@@ -1,15 +1,21 @@
 import React from "react"
 import { Helmet } from "react-helmet"
 
-const Meta = () => (
-  <Helmet>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootswatch/4.3.1/litera/bootstrap.min.css"/>
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous"></link>
-    <title>My Hot Shit Blog</title>
-  </Helmet>
-)
+class Meta extends React.Component {
+  render () {
+    return (
+      <Helmet>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootswatch/4.3.1/litera/bootstrap.min.css"/>
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous"></link>
+        <title>{this.props.siteTitle}</title>
+        <meta name="description" content={this.props.siteDescription} />
+        <link rel="canonical" href={this.props.siteUrl} />
+      </Helmet>
+    )
+  }
+}
 
 export default Meta
